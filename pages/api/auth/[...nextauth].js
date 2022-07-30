5344deccef7ded86dd42cabe98376f42d2992674
Blaike Bradford <bb494v@att.com>
2022-07-30 01:24:33 -0500

    Spotify web-api successful login through api

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth/next";
 import SpotifyProvider from "next-auth/providers/spotify";
-import { refreshAccessToken } from "spotify-web-api-node/src/server-methods";
-import { LOGIN_URL } from "../../../lib/spotify";
 import spotifyAPI, {LOGIN_URL} from '../../../lib/spotify';
 
 async function refreshAccessToken(token)
@@ -12,20 +10,33 @@ async function refreshAccessToken(token)
       spotifyAPI.setRefreshToken(token.refreshToken);
 
       //deconstruct the response
+      //sending both accessToken and refreshToken back to spotifyAPI
+      // provides a new accessToken
       const { body: refreshedToken } = await spotifyAPI.refreshAccessToken();
 
+      console.log("REFRESHED TOKEN IS", refreshedToken)
+
+      return {
+        ...token,
+        accessToken: refreshedToken.access_token,
+        accessTokenExpires: Date.now + refreshedToken.expires_in * 1000, // = 1 hour as 3600 returns from 
+        //spotify api
+        refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+        // Replace if new one came back else fall back to old refresh token
+      };
     }catch(error)
     {
-      console.log(error)
+      console.error(error)
       return {
         ...token, 
-        error: "RefreshAccessTokenError"
-      }
+        error: "RefreshAccessTokenError",
+      };
     }
 }
 
 
 export default NextAuth ({
+  //configure 1 or more auth providers
     providers: [
         SpotifyProvider({
           clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
@@ -42,16 +53,20 @@ export default NextAuth ({
       async jwt({token, account, user}){
         // check conditions
         // initial sign in return this token
+        // refresh Token Rotation -> NextAuth
         if(account && user){
+          //first time signed In
           return {
             ...token, 
             accessToken: account.access.token,
             refreshToken: account.refresh_token,
             username: account.providerAccountId,
             accessTokenExpires: account.expires_at * 1000,
+            //handling expire times in miliseconds 
           }
         }
         //refresh token
+        // persistance page checking if token is valid
         // Return previous token if access token has not expired yet
         if(Date.now() < token.accessTokenExpires)
         {
@@ -61,8 +76,17 @@ export default NextAuth ({
 
         //Access token has expired, so we need to update
         console.log("ACCESS TOKEN HAS EXPIRED, REFRESHING...");
-        return await refreshAccessToken(token)
+        return await refreshAccessToken(token);
       },
+
+      async session({session, token}){
+        session.user.accessToken = token.accessToken;
+        session.user.refreshToken = token.refreshToken;
+        session.user.username = token.username;
+
+        //connected the token to user session
+        return session;
+      }
     }
 });
 
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { getProviders, signIn } from "next-auth/react";
 
-const login = () => {
+function Login({providers}){
   return (
-    <div>login Page</div>
-  )
+    // items center = horiz axis justify-center - vert axis
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center"> 
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt=""/>
+      {Object.values(providers).map((provider) => ( 
+
+        <div>
+          <button className="bg-[#18D860] text-white p-5 rounded-full" 
+          onClick={() => signIn(provider.id, {callbackUrl: "/"})}>Login with provider {provider.name}</button>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default login
+export default Login;
+
+export async function getServerSideProps() {
+  
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
